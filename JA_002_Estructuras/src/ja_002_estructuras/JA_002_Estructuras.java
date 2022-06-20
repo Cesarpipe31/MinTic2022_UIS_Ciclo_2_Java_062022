cb46bf6a438b6c3002b5a61a4d76c84d43756c4a
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2022-06-20 11:13:27 -0500

    UIS MinTic 2022 Java

--- JA_002_Estructuras/src/ja_002_estructuras/JA_002_Estructuras.java
+++ JA_002_Estructuras/src/ja_002_estructuras/JA_002_Estructuras.java
@@ -20,8 +20,11 @@ public class JA_002_Estructuras
     public static void main(String[] args) 
     {
         // TODO code application logic here
+        
+/*
         Scanner leer = new Scanner(System.in);
         int numero;
+
         String palabra;
         System.out.print("Escriba un número: ");
         numero = leer.nextInt();
@@ -29,6 +32,8 @@ public class JA_002_Estructuras
         palabra = leer.next();
         
         System.out.println("El númerpo es: " + numero + " y la palabra es: " + palabra);
+       
+//  Bucle if
         
         if(numero > 100)
         {
@@ -42,5 +47,32 @@ public class JA_002_Estructuras
         {
             System.out.println ("El número es menor que 100");
         }
+*/
+
+//  Bucle for
+
+/*)
+        
+        int numero;
+        for (numero = 1; numero <= 10; numero = numero + 1)
+        {
+            System.out.println ("Imprimiendo número con for " + numero);
+        }
     }
+}
+
+*/
+        
+//  Bucle while
+        
+    int numero;
+    numero = 1;
+    while (numero <= 10)
+    {
+        System.out.println("Imprimiento con while numero " + numero);
+        numero = numero + 2;
+    }
+    
+    }
+
 }
